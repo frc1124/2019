15eda9a3d9a005bdb6e7f08f63bb04030823c4cb
Chris Barnes <cbarnes547@gmail.com>
2019-01-05 14:32:37 -0500

    Begining Drive Train Setup

---
+++ Uberbots2019/src/main/java/frc/robot/commands/ArcadeDriveJoystick.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import frc.robot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ArcadeDriveJoystick extends Command {
+
+  public ArcadeDriveJoystick(){
+    requires(Robot.driveTrain);
+
+    setInterruptible(true);
+  }
+
+  @Override
+  protected void initialize() {
+    
+  }
+
+  @Override
+  protected void execute() {
+    Joystick js = Robot.oi.getJoystick();
+    
+    Robot.driveTrain.drive(js);
+  }
+
+    @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  protected void end() {
+    Robot.driveTrain.stop();
+  }
+
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
---
+++ Uberbots2019/src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import frc.robot.RobotMap;
+import frc.robot.commands.ArcadeDriveJoystick;
+
+public class Drive extends Subsystem{
+
+    protected DifferentialDrive diffDrive;
+
+    protected final double THROTTLE = .75;
+
+    public Drive(){
+        super("Drive");
+
+    }
+
+    @Override
+    public void initDefaultCommand(){
+        setDefaultCommand(new ArcadeDriveJoystick());
+    }
+
+    public void drive(double move, double rotate){
+        diffDrive.arcadeDrive(move, rotate);
+    }
+
+    public void drive(Joystick joystick){
+        this.drive(THROTTLE * -joystick.getY(), THROTTLE * -joystick.getX());
+    }
+
+    public void stop(){
+        diffDrive.arcadeDrive(0, 0);
+    }
+
+}
