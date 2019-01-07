d0571164cba7da9dc0b3e1fb752823794d8323cf
Chris Barnes <cbarnes547@gmail.com>
2019-01-07 15:38:41 -0500

    Talon Setup
    Added talon and temp addresses

--- Uberbots2019/src/main/java/frc/robot/RobotMap.java
+++ Uberbots2019/src/main/java/frc/robot/RobotMap.java
@@ -8,4 +8,10 @@
 package frc.robot;
 
 public class RobotMap {
+
+    //Can addresses
+    public static final int LEFT_1 = 0;
+    public static final int LEFT_2 = 2;
+    public static final int RIGHT_1 = 1;
+    public static final int RIGHT_2 = 3;
 }
--- Uberbots2019/src/main/java/frc/robot/subsystems/Drive.java
+++ Uberbots2019/src/main/java/frc/robot/subsystems/Drive.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 import frc.robot.RobotMap;
 import frc.robot.commands.ArcadeDriveJoystick;
@@ -14,14 +15,23 @@ public class Drive extends Subsystem{
     protected static WPI_TalonSRX leftBack, leftFront;
     protected static WPI_TalonSRX rightBack, rightFront;
 
+    protected static SpeedControllerGroup left, right;
+
     protected static DifferentialDrive diffDrive;
 
     protected final double THROTTLE = .75;
 
     public Drive(){
         super("Drive");
+        leftBack = new WPI_TalonSRX(RobotMap.LEFT_1); //Check device numbers
+        leftFront = new WPI_TalonSRX(RobotMap.LEFT_2);
+        left = new SpeedControllerGroup(leftBack, leftFront);
 
+        rightBack = new WPI_TalonSRX(RobotMap.RIGHT_1);
+        rightFront = new WPI_TalonSRX(RobotMap.RIGHT_2);
+        right = new SpeedControllerGroup(rightBack, rightFront);
 
+        diffDrive = new DifferentialDrive(left, right);
     }
 
     @Override
