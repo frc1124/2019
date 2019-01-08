6d2e8915cf769ab89a5809b92a6c51b518ab0caf
milanos <mmshah2004@gmail.com>
2019-01-08 18:48:19 -0500

    Added Gyro/Accel methods to Drive on Tue Jan  8 18:48:19 EST 2019

--- Uberbots2019/src/main/java/frc/robot/subsystems/Drive.java
+++ Uberbots2019/src/main/java/frc/robot/subsystems/Drive.java
@@ -8,12 +8,18 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import frc.robot.RobotMap;
 import frc.robot.commands.ArcadeDriveJoystick;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Drive extends Subsystem{
 
     protected static WPI_TalonSRX leftBack, leftFront;
     protected static WPI_TalonSRX rightBack, rightFront;
+
+	private AnalogGyro gyro;
+	private BuiltInAccelerometer accel;
 
     protected static SpeedControllerGroup left, right;
 
@@ -32,6 +38,11 @@ public class Drive extends Subsystem{
         right = new SpeedControllerGroup(rightBack, rightFront);
 
         diffDrive = new DifferentialDrive(left, right);
+
+		gyro = new AnalogGyro(RobotMap.GYRO);
+		gyro.initGyro();
+		
+		accel = new BuiltInAccelerometer();
     }
 
     @Override
@@ -50,5 +61,42 @@ public class Drive extends Subsystem{
     public void stop(){
         diffDrive.arcadeDrive(0, 0);
     }
+
+	public void resetGyro(){
+		gyro.reset();
+	}
+	
+	/**
+	 * Goes beyond 360 degrees
+	 * @return absolute full angle that is beyond 360 degrees after 1 rotation
+	 */
+	public double getFullAngle(){
+		return gyro.getAngle();
+	}
+	
+	/**
+	 * Angle is between 0 and 360 degrees
+	 */
+	public double getAngle(){
+		return gyro.getAngle() / 360.0;
+	}
+	
+	public double getAngularRate(){
+		return gyro.getRate();
+	}
+	
+	// accelerometer methods
+	
+	public double getAccelX(){
+		return accel.getX();
+	}
+
+	public double getAccelY(){
+		return accel.getY();
+	}
+
+	public double getAccelZ(){
+		return accel.getZ();
+	}
 
 }
