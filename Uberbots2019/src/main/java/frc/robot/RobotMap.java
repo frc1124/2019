84cda0ad48011c5f029fec86d64501292b832149
Chris Barnes <cbarnes547@gmail.com>
2019-01-19 11:00:50 -0500

    Talon addresses

--- Uberbots2019/src/main/java/frc/robot/RobotMap.java
+++ Uberbots2019/src/main/java/frc/robot/RobotMap.java
@@ -10,8 +10,8 @@ package frc.robot;
 public class RobotMap {
 
     // Can addresses
-    public static final int LEFT_1 = 0;
-    public static final int LEFT_2 = 3;
+    public static final int LEFT_1 = 3;
+    public static final int LEFT_2 = 0;
     public static final int RIGHT_1 = 2;
     public static final int RIGHT_2 = 4;
 
